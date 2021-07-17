84ebb9f3b0057740d54eb83d64d4c0cb430a1d11
Jess <jmvsevilleja@gmail.com>
2021-07-17 11:07:36 +0800

    Added touchable styles

--- App.js
+++ App.js
@@ -1,18 +1,47 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet, Text,
+  //TouchableWithoutFeedback,
+  //TouchableOpacity,
+  TouchableHighlight,
+  Image, View, SafeAreaView
+} from 'react-native';
 
 export default function App() {
   let x = 1;
   console.log('App executed');
 
+  const handlePress = () => {
+    console.log("Text Pressed")
+  }
+  const handleImagePress = () => {
+    console.log("Image Pressed")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!</Text>
+    <SafeAreaView style={[styles.container, otherStyle]}>
+      <Text>Hello World</Text>
+      <Text onPress={handlePress}>Click Me!</Text>
+      {/* Static Image */}
+      <Image source={require("./assets/favicon.png")} />
+      {/* Network Image */}
+      <TouchableHighlight onPress={handleImagePress}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://reactjs.org/logo-og.png",
+          }}
+        />
+      </TouchableHighlight>
+
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+const otherStyle = {backgroundColor: "#ccc"}
 
 const styles = StyleSheet.create({
   container: {
