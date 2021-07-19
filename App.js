10b1ed95ecff431044b801c4f9041aafa35ae4d0
Jess <jmvsevilleja@gmail.com>
2021-07-19 10:39:13 +0800

    Added hooks dimension

--- App.js
+++ App.js
@@ -6,17 +6,18 @@ import {
   //TouchableOpacity,≠
   TouchableHighlight,
   Image, View, SafeAreaView,
-  Button, Platform, Dimensions,
+  Button, Platform, Dimensions,// StatusBar
 } from 'react-native';
-import {useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
+// Hooks
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
 
 export default function App() {
   let x = 1;
   console.log('App executed');
   // react dimensions
-  console.log('get screen:', Dimensions.get("screen"));
+  console.log('Get Screen:', Dimensions.get("screen"));
   // hooks dimensions
-  console.log('useDimension: ',useDimensions());
+  console.log('useDimension: ', useDimensions());
   console.log('useOrientation', useDeviceOrientation());
 
   const handlePress = () => {
@@ -25,14 +26,26 @@ export default function App() {
   const handleImagePress = () => {
     console.log("Image Pressed")
   }
+  // object destructuring
+  const {landscape} = useDeviceOrientation();
 
   return (
+    // SafeAreaView is IOS only
     <SafeAreaView style={[styles.container, overrideStyle]}>
+      <View style={{
+        backgroundColor: "#f00",
+        width: "100%",
+        height: landscape ? "50%" : "30%",
+      }}
+      >
+
+      </View>
       <Text>Hello World</Text>
       <Text onPress={handlePress}>Click Me!</Text>
       {/* Static Image */}
       <Image source={require("./assets/favicon.png")} />
       {/* Network Image */}
+
       <TouchableHighlight onPress={handleImagePress}>
         <Image
           source={{
@@ -42,32 +55,26 @@ export default function App() {
           }}
         />
       </TouchableHighlight>
-      <Button 
-      color="blue" 
-      title="Button" 
-      onPress={()=>alert('Button Pressed')} 
+      <Button
+        color="blue"
+        title="Button"
+        onPress={() => alert('Button Pressed')}
       />
-      <View style={{
-        backgroundColor:"#f00",
-        width: "100%",
-        height: 70,}} 
-      >
-        
-      </View>
-      
+      {/* A string, either: 'auto', 'inverted', 'light', or 'dark'. */}
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
-const overrideStyle = {backgroundColor: "#ccc"} 
+const overrideStyle = {backgroundColor: "#ccc"}
 // OS
-console.log(Platform.OS)
-console.log(StatusBar.height) // null in IOS
+console.log('Platform OS:', Platform.OS)
+// Works only in StatusBar react-native
+console.log('Statusbar Height', StatusBar.currentHeight) // null in IOS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    paddingTop: 10 // ios not affected
+    paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0 // ios not affected
   },
 });
