85deca82796f2de140da22a260860a1f38a72c36
Jess <jmvsevilleja@gmail.com>
2021-07-19 14:31:42 +0800

    Added flex and assets

--- App.js
+++ App.js
@@ -30,6 +30,58 @@ export default function App() {
   const {landscape} = useDeviceOrientation();
 
   return (
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        // width: 100,
+        // height: 100,
+        flexDirection: "row", //column,row-reverse
+        justifyContent: "center", // main
+        alignItems: "center", // secondary
+        // alignContent: "center", // applicable to flexWrap
+        //flexWrap: "wrap"
+
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          //flex: 1,
+          width: 100,
+          height: 100,
+          // flexShrink: 1, // same as flex -1
+          // flexBasis: 100, //width or height
+          // flexGrow: 1, // same as flex
+          // alignSelf: "flex-start"
+        }} />
+      <View
+        style={{
+          backgroundColor: "gold",
+          //flex: 1,
+          width: 100,
+          height: 100,
+          position: "relative", // absolute by parent
+          top: -20,
+          left: 20,
+        }} />
+      <View
+        style={{
+          backgroundColor: "red",
+          //flex: 1,
+          width: 100,
+          height: 100,
+        }} />
+      {/* <View
+        style={{
+          backgroundColor: "yellow",
+          //flex: 1,
+          width: 100,
+          height: 100,
+        }} /> */}
+    </View>
+  );
+  return (
     // SafeAreaView is IOS only
     <SafeAreaView style={[styles.container, overrideStyle]}>
       <View style={{
@@ -43,7 +95,7 @@ export default function App() {
       <Text>Hello World</Text>
       <Text onPress={handlePress}>Click Me!</Text>
       {/* Static Image */}
-      <Image source={require("./assets/favicon.png")} />
+      <Image source={require("./app/assets/favicon.png")} />
       {/* Network Image */}
 
       <TouchableHighlight onPress={handleImagePress}>
